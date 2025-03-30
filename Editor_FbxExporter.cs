3d69b512c9acd41f11a4cecd60dfae55de4ea24f
向日葵花语 <1575607629@qq.com>
2025-03-30 19:53:17 +0800

    Add files via upload
    将Unity中模型导出为FBX格式，搭配FBXExporter.dll使用

---
+++ Editor_FbxExporter.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Text;
+
+public class Editor_FbxExporter : EditorWindow
+{
+
+    private static Editor_FbxExporter _window;
+
+    private string fbxname = "";
+    private string path = "";
+    GameObject[] meshObjs = null;
+
+    [MenuItem ("Tools/导出模型")]
+    public static void GUIDRefReplaceWin()
+    {
+        Rect wr = new Rect (0, 0, 300, 200);
+        // true 表示不能停靠的
+        _window = (Editor_FbxExporter)GetWindowWithRect (typeof (Editor_FbxExporter),wr, true, "导出FBX");
+        _window.Show ();
+
+    }
+    void OnGUI()
+    {
+        if (meshObjs != Selection.gameObjects)
+        {
+            fbxname = "";
+            meshObjs = Selection.gameObjects;
+        }
+        
+        if (meshObjs.Length!=0)
+        {
+            GUILayout.Label ("当前选择物体名字 : " + meshObjs[0].name);
+            if (fbxname=="")
+            {
+                fbxname = "Fbx_"+meshObjs[0].name;
+            }
+            fbxname = EditorGUILayout.TextField ("导出FBX名字：", fbxname);
+
+            GUILayout.Label ("导出路径为Assets目录");
+            if (GUILayout.Button ("开始导出"))
+            {
+                FBXExporter.ExportFBX (path, fbxname, meshObjs, true);
+            }
+        }
+        else
+        {
+            fbxname = "";
+            GUILayout.Label ("先选中物体");
+        }
+    }
+}
